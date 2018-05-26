fe2beb6aba3b99f2c97acc7fe693fa76ad91c8b9
Anton Artamonov <aartamonov@axmit.com>
2018-05-27 02:26:25 +0300

    fix it

--- api/configDB.js
+++ api/configDB.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
 const config = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'karelia2018',
-  database: 'mnt_users_db'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB
 });
 
 module.exports = config;
