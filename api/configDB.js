f197c79df91a49b64163e5d9df8b55237aa3bdc8
Anton Artamonov <aartamonov@axmit.com>
2018-05-27 01:47:36 +0300

    env

--- api/configDB.js
+++ api/configDB.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql');
 const config = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB
